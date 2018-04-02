a005e5fb572d3540abbc82db22a37d6fdfd8b318
ssri74 <37617244+ssri74@users.noreply.github.com>
2018-04-02 15:00:22 +1000

    -

--- CareerTree/CareerTree/index.php
+++ CareerTree/CareerTree/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 
 <head>
@@ -114,35 +114,6 @@
 		}
 	</script>
 </body>
-<<<<<<< HEAD
-<SCRIPT>
-function passWord() {
-var testV = 1;
-var pass1 = prompt('Please Enter Your Password','');
-while (testV < 3) {
-if (!pass1) 
-history.go(-1);
-if (pass1.toLowerCase() == "ideservehd") {
-alert('You Got it Right!');
-window.location.replace('p1.php');
-break;
-} 
-testV+=1;
-var pass1 = 
-prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
-}
-if (pass1.toLowerCase()!="password" & testV ==3) 
-history.go(-1);
-return " ";
-} 
-</SCRIPT>
-<CENTER>
-<FORM>
-<input type="button" value="Enter Protected Area" onClick="passWord()">
-</FORM>
-</CENTER>
-=======
 
-
->>>>>>> manali_branch
+>>>>>>> manali_branch-->
 </html>
