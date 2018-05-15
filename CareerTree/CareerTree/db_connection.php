0422972be96de87aa8d59f375993959ebbc31aa9
ssri74 <37617244+ssri74@users.noreply.github.com>
2018-05-15 10:02:08 +1000

    change DB connection to PRD database

--- CareerTree/CareerTree/db_connection.php
+++ CareerTree/CareerTree/db_connection.php
@@ -2,12 +2,12 @@
 function OpenCon()
 {
     /*DB connection*/
-    $dbhost = 'careertreetest.cytukzawpi8t.ap-southeast-2.rds.amazonaws.com';
-    $dbuser = 'careertreetest';
-    $dbpass = 'careerree2018';
-    $port = 5432;
-    $dbname ='ctdb_test';
-    $conn = "host=careertreetest.cytukzawpi8t.ap-southeast-2.rds.amazonaws.com port=5432 dbname=ctdb_test user=careertreetest password=careertree2018";
+    //$dbhost = 'careertreetest.cytukzawpi8t.ap-southeast-2.rds.amazonaws.com';
+    //$dbuser = 'careertreetest';
+    //$dbpass = 'careerree2018';
+    //$port = 5432;
+    //$dbname ='ctdb_test';
+    $conn = "host=careertreeprd.cytukzawpi8t.ap-southeast-2.rds.amazonaws.com port=5432 dbname=ctdb_prd user=careertreeprd password=careertree2018";
     $dbconn = pg_connect($conn);
     if(! $conn )
     {
