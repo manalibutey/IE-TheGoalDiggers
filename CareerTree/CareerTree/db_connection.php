a35ec7de8d06022cb595c306252120ae96702e93
ssri74 <37617244+ssri74@users.noreply.github.com>
2018-05-16 12:35:52 +1000

    change db connect

--- CareerTree/CareerTree/db_connection.php
+++ CareerTree/CareerTree/db_connection.php
@@ -7,7 +7,8 @@ function OpenCon()
     //$dbpass = 'careerree2018';
     //$port = 5432;
     //$dbname ='ctdb_test';
-    $conn = "host=careertreeprd.cytukzawpi8t.ap-southeast-2.rds.amazonaws.com port=5432 dbname=ctdb_prd user=careertreeprd password=careertree2018";
+    //$conn = "host=careertreeprd.cytukzawpi8t.ap-southeast-2.rds.amazonaws.com port=5432 dbname=ctdb_prd user=careertreeprd password=careertree2018";
+    $conn = "host=careertreetest.cytukzawpi8t.ap-southeast-2.rds.amazonaws.com port=5432 dbname=ctdb_test user=careertreetest password=careertree2018";
     $dbconn = pg_connect($conn);
     if(! $conn )
     {
